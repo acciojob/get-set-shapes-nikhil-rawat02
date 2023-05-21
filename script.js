60ee9a5c4b37c5e48451db083dbeb0f637459621
Nikhil Rawat <117645790+nikhil-rawat02@users.noreply.github.com>
2023-05-21 11:26:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 //complete this code
 class Rectangle {
 	constructor(width, height){
-		this._width = width;
-		this._height = height;
+		this.width = width;
+		this.height = height;
+	}
+	get width(){
+		return this.width;
+	}
+	get height(){
+		return this.height;
 	}
 	getArea(){
-		return this._width * this._height;
+		return this.width * this.height;
 	}
 }
 
